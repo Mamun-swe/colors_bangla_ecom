eced9fb84f13f8f93c8aecd378726340c55f16ad
Mamun-swe <mhmamun166009@gmail.com>
2020-10-21 13:25:37 +0600

    loading gif add

--- src/Components/Modal/HomeLoadingModal.js
+++ src/Components/Modal/HomeLoadingModal.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../../styles/modal.scss';
-
-import BannerImg from '../../assets/static/offer_banner.jpg';
+import LoadingGif from '../../assets/static/loading.gif';
 
 const HomeLoadingModal = () => {
     return (
         <div className="home-loading-modal">
             <div className="flex-center flex-column">
-                <div className="card border-0 rounded-0 shadow p-0">
-                    <img src={BannerImg} className="img-fluid" alt="..." />
+                <div className="card border-0 rounded-0 p-0">
+                    <img src={LoadingGif} className="img-fluid" alt="..." />
                 </div>
             </div>
         </div>
