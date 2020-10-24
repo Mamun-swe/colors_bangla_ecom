7b46010d3dd309f12938a8dd7a60908dac99f7d0
Mamun-swe <mhmamun166009@gmail.com>
2020-10-24 11:37:47 +0600

    okk

--- src/Components/Category/CategoryComponent.js
+++ src/Components/Category/CategoryComponent.js
@@ -7,21 +7,22 @@ const CategoryComponent = ({ categories }) => {
         <div className="category-component">
             <div className="container">
                 <div className="row">
+                    <div className="col-12 p-2 p-sm-0">
 
-                    {categories.length > 0 && categories.slice(0, 6).map((category, i) =>
-                        <div className="col-6" key={i}>
-                            <Link to={`/category/${category.id}`}>
+                        {categories.length > 0 && categories.slice(0, 6).map((category, i) =>
+                            <Link to={`/category/${category.id}`} key={i}>
                                 <div className="card border-0 rounded-0 category-card">
                                     <img src={category.image} className="img-fluid" alt="..." />
-                                    <div className="overlay">
+                                    {/* <div className="overlay">
                                         <div className="flex-center flex-column">
                                             <h2 className="mb-0">{category.name.slice(0, 6)}</h2>
                                         </div>
-                                    </div>
+                                    </div> */}
                                 </div>
                             </Link>
-                        </div>
-                    )}
+                        )}
+
+                    </div>
                 </div>
             </div>
         </div>
