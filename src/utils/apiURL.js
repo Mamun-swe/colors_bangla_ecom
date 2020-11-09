3fd53c54bce673b89a87c43f16defcd300e1f1ee
Mamun-swe <mhmamun166009@gmail.com>
2020-11-09 17:10:17 +0600

    v1 done

--- src/utils/apiURL.js
+++ src/utils/apiURL.js
@@ -1,5 +1,3 @@
-// export const apiURL = "https://colourbangladev.xyz/api/";
 
-export const apiURL = "http://127.0.0.1:8000/api/";
+export const apiURL = "https://api.urfashionsbd.com/public/api/";
 
-// http://127.0.0.1:8000/
