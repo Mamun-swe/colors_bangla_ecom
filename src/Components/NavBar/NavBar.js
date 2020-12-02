4f274ac36fb8a21047ce758ea7658cca4fe57593
Mamun-swe <mhmamun166009@gmail.com>
2020-12-02 12:58:50 +0600

    Search box added

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -132,6 +132,11 @@ const NavBar = () => {
                                         <MegaMenuComponent items={categories} />
                                     </div>
                                 </div>
+                                {/* Search Component */}
+                                <div className="pl-2 pl-lg-0" style={{ paddingTop: 4 }}>
+                                    <SearchComponent />
+                                </div>
+
                                 {/* Cart Button */}
                                 <div className="pr-2 ml-auto">
                                     <div className="cart-box">
@@ -161,157 +166,6 @@ const NavBar = () => {
                     </div>
 
                 </div>
-
-                {/* Mobile Navbar */}
-                {/* <div className="mobile-nav d-lg-none">
-                <div className="top-nav p-3">
-                    <div className="d-flex">
-                        <div>
-                            <Link to="/">
-                                <img src={Logo} className="img-fluid" alt="..." />
-                            </Link>
-                        </div>
-                        <div className="flex-fill px-2">
-                            <SearchComponent />
-                        </div>
-                        <div className="pr-2">
-                            <div className="cart-box">
-                                <Link to="/shopping-cart"
-                                    type="button"
-                                    className="btn rounded-circle shadow-none cart-btn">
-                                    <Icon icon={ic_add_shopping_cart} size={22} />
-                                </Link>
-                                {
-                                    cartProducts ?
-                                        <small>{cartProducts.length}</small>
-                                        : <small>0</small>
-                                }
-                            </div>
-                        </div>
-                        <div>
-                            <button
-                                type="button"
-                                className="btn rounded-circle shadow-none p-1 bar-btn"
-                                onClick={() => setOpen(true)}>
-                                <Icon icon={ic_dehaze} size={25} />
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div className="top-nav-break d-lg-none"></div> */}
-
-                {/* Menu */}
-                {/* <div className={open ? "mobile-menu open-menu" : "mobile-menu"}>
-                    <div className="content-header p-3 text-right">
-                        <button
-                            type="button"
-                            className="btn rounded-circle shadow-none p-1"
-                            onClick={() => setOpen(false)}>
-                            <Icon icon={ic_close} size={25} />
-                        </button>
-                    </div>
-                    <div className="content-body px-4">
-                        <Link to="/">home</Link>
-                        <Link to="/shop">shop</Link>
-                        <Link to="/contact">contact</Link>
-                        <Link to="/sign-in">my account</Link>
-                    </div>
-                </div>
-            </div> */}
-
-                {/* Desktop Nav One */}
-                {/* <div className="desktop-nav-one d-none d-lg-block">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="d-flex py-4">
-                                <div className="links">
-                                    <ul>
-                                        <li>
-                                            <Link to="/">
-                                                <img src={Logo} className="img-fluid" alt="..." />
-                                            </Link>
-                                        </li>
-                                        <li><NavLink exact activeClassName="is-active" to="/">home</NavLink></li>
-                                        <li><NavLink exact activeClassName="is-active" to="/shop">shop</NavLink></li>
-                                        <li><NavLink exact activeClassName="is-active" to="/contact">contact</NavLink></li>
-                                    </ul>
-                                </div>
-                                <div className="ml-auto account-links">
-                                    <ul>
-                                        <li>
-                                            <Link to="/sign-in">
-                                                <div className="d-flex">
-                                                    <div><Icon icon={user} size={20} color="#000" className="border p-1" /></div>
-                                                    <div className="pl-2">
-                                                        <p className="mb-0">my account</p>
-                                                        <small>sing in/join</small>
-                                                    </div>
-                                                </div>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link to="/shopping-cart">
-                                                <div className="d-flex">
-                                                    <div><Icon icon={ic_add_shopping_cart} size={20} color="#000" className="border p-1" /></div>
-                                                    <div className="pl-2">
-                                                        <p className="mb-0">my cart</p>
-                                                        {
-                                                            cartProducts ?
-                                                                <small>{cartProducts.length} items</small>
-                                                                : <small>0 items</small>
-                                                        }
-                                                    </div>
-                                                </div>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-
-                {/* Desktop Nav Black */}
-                {/* <div className="desktop-nav-black d-none d-lg-block py-3">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="d-flex">
-                                <div>
-                                    <div className="d-flex">
-                                        <div className="category-btn-box pr-2">
-                                            <button
-                                                type="button"
-                                                className="btn shadow-none"
-                                                onClick={() => setIsShow(!isShow)}
-                                            >
-                                                <Icon icon={text_left} size={18} className="mr-2" />Categories
-                                            </button>
-                                            {isShow ?
-                                                <MegaMenuComponent items={categories} />
-                                                : null}
-                                        </div>
-
-                                        <div className="search-desktop">
-                                            <SearchComponent />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="ml-auto">
-                                    <ul>
-                                        <li><a href="https://www.facebook.com/urfashion21"><Icon icon={facebook} size={15} /></a></li>
-                                        <li><a href="https://www.instagram.com/urfasshion21"><Icon icon={instagram} size={15} /></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-         */}
-
             </div>
             {/* Mobile Menu Items */}
             <div
@@ -320,7 +174,6 @@ const NavBar = () => {
             </div>
             <div className={isOpen ? "mobile-menu d-lg-none open-mobile-menu" : "mobile-menu d-lg-none hide-mobile-menu"}>
                 {/* Menu Body */}
-
                 <div className="menu-body p-3">
                     <ul>
                         <li><NavLink exact activeClassName="is-Active" to="/">home<Icon icon={ic_keyboard_arrow_right} className="float-right" size={20} /></NavLink></li>
